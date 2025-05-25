adb2e84b23f4a13b2e9ffc3384e748b8510e7276
Dami <damianvlasaty01@.com>
2025-05-25 14:10:05 +0200

    Second commit

--- src/main/java/demotest/SdaTest.java
+++ src/main/java/demotest/SdaTest.java
@@ -3,6 +3,7 @@ package demotest;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -20,8 +21,12 @@ public class SdaTest {
     public void sdaTest(){
         //Toto mi pojde na stranku tutorialpoint
         driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+        //Toto mi zvetsi obrazovku
+        driver.manage().window().setSize(new Dimension(1936, 1048));
         //Toto mi najde na stranke prvok a vzpise meno
         driver.findElement(By.id("name")).sendKeys("Jenda");
+        //Toto mi najde na stranke prvok a vzpise email
+        driver.findElement(By.id("email")).sendKeys("email@email.cz");
 
 
     }
