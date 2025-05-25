5ac4b2b73b50a24ea660ad31d4ccd5c6583270cf
Dami <damianvlasaty01@.com>
2025-05-25 13:52:42 +0200

    Second commit

---
+++ src/main/java/demotest/SdaTest.java
@@ -0,0 +1,31 @@
+package demotest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SdaTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\damia\\z4086\\Work\\SDA\\z4086\\SdaFinalTest\\src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void sdaTest(){
+        //Toto mi pojde na stranku tutorialpoint
+        driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+        //Toto mi najde na stranke prvok a vzpise meno
+        driver.findElement(By.id("name")).sendKeys("Jenda");
+
+
+    }
+
+
+
+}
