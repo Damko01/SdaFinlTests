fdfbfda1c6496e63c4b0f9b6a8ac18fd3ca9bdfd
Dami <damianvlasaty01@.com>
2025-06-02 20:48:31 +0200

    Thirds commit

--- src/main/java/demotest/SdaTest.java
+++ src/main/java/demotest/SdaTest.java
@@ -5,10 +5,19 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import org.openqa.selenium.support.ui.Select;
 
 import javax.swing.text.html.Option;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
 
 public class SdaTest {
 
@@ -21,7 +30,7 @@ public class SdaTest {
     }
 
     @Test
-    public void sdaTest(){
+    public void sdaTest() throws InterruptedException {
         //Toto mi pojde na stranku tutorialpoint
         driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
         //Toto mi zvetsi obrazovku
@@ -33,12 +42,19 @@ public class SdaTest {
         driver.findElement(By.id("gender")).click();
         driver.findElement(By.id("mobile")).sendKeys("+41 76 305 48 68");
         driver.findElement(By.id("dob")).sendKeys("08.02.2001");
-        driver.findElement(By.xpath("//label[text()='Reading']/preceding-sibling::input")).click();
+        driver.findElement(By.xpath("//label[text()='Sports']/preceding-sibling::input")).click();
         driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[9]/div/textarea")).sendKeys("Alte Luzernstr. 15");
+        // Vyber štát "NCR"
+        Select stateSelect = new Select(driver.findElement(By.id("state")));
+        stateSelect.selectByVisibleText("NCR");
+        // Počkaj chvíľku, aby sa načítali mestá (alebo použi wait podľa potreby)
+        Thread.sleep(1000); // jednoduché riešenie, vhodné len na testy
+        // Vyber mesto "Delhi" (alebo iné dostupné pre NCR)
+        Select citySelect = new Select(driver.findElement(By.id("city")));
+        citySelect.selectByVisibleText("Agra");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+
 
 
     }
-
-
-
 }
